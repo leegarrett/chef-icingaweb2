80f597e8afc9cd1ae80f40c5757c4ea9443f15d2
Lee Garrett <lgarrett@rocketjump.eu>
2018-11-07 11:32:48 +0100

    Bump dependency on apache2 cookbook
    This one supports newer OS releases. No semantic changes in the cookbook needed.

--- metadata.rb
+++ metadata.rb
@@ -10,7 +10,7 @@ chef_version '>= 12.1' if respond_to?(:chef_version)
 issues_url 'https://github.com/Icinga/chef-icingaweb2/issues' if respond_to?(:issues_url)
 source_url 'https://github.com/Icinga/chef-icingaweb2' if respond_to?(:source_url)
 
-depends 'apache2', '~> 3.3.0'
+depends 'apache2', '~> 5.2.1'
 depends 'yum-epel', '>= 2.1.1'
 depends 'yum-scl', '>= 0.2.0'
 depends 'apt', '>= 5.0.1'
